738546601c821b3a52a52210ba4df2f7247a8d8f
sjeevankumar <sjeevankumar89@gmail.com>
2024-10-16 15:23:23 +0530

    feat:add trending videos section

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -13,12 +13,13 @@ import { images } from "../../constants"
 import SearchInput from "../../components/SerachInput"
 import Trending from "../../components/Trending"
 import EmptyState from "../../components/EmptyState"
-import { getAllPosts } from "../../lib/appwrite"
+import { getAllPosts, getLatestPosts } from "../../lib/appwrite"
 import useAppwrite from "../../lib/useAppwrite"
 import VideoCard from "../../components/VideoCard"
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts)
+  const { data: latestPosts } = useAppwrite(getLatestPosts)
   const [refreshing, setRefreshing] = useState(false)
 
   const onRefresh = async () => {
@@ -26,7 +27,6 @@ const Home = () => {
     await refetch()
     setRefreshing(false)
   }
-  console.log(posts, "p")
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -58,7 +58,7 @@ const Home = () => {
               <Text className="text-gray-100 text-lg font-pregular mb-3">
                 Lates Videos
               </Text>
-              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
